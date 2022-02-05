925d0ae6ff5374175a70fe76cd56cce0bddadc12
Marcelo Castilho <mardinez@gmail.com>
2022-02-05 18:15:49 -0300

    Primeiro Commit

---
+++ Operacoes_Matematicas.py
@@ -0,0 +1,48 @@
+# Adição (+)
+x = 2 + 2
+print(x)
+
+# Subtração (-)
+x = 2 - 2
+print(x)
+
+# Multiplicação (*)
+x = 2 * 3
+print(x)
+
+# Divisão (/) (Se utilizar o / duas vezes resulta em apenas numeros inteiros)
+x = int(10 / 3)
+y = 10 // 3
+print(float(x))
+print(int(y))
+
+# Exponenciação (**)
+x = 2 ** 3
+print(x)
+
+# Resto da divisão (%)
+x = 10 % 3
+y = 10 % 2
+print(x)
+print(y)
+
+# -------------------------------------- #
+
+# Expressões Matemáticas
+x = 5 * 2 + 3 * 2
+print(x)
+# é igual a isso =
+y = (5 * 2) + (3 * 2)
+print(y)
+# é diferente disso <>
+z = 5 * (2 + 3) * 2
+print(z)
+
+# -------------------------------------- #
+
+# variável
+
+x = 5 * 2
+print(x)
+y = x + 3 * 2
+print(y)
---
+++ Variaveis.py
@@ -0,0 +1,14 @@
+ano_atual = 2022
+ano_fabricacao = 2003
+km_total = 44410.0
+
+km_media = km_total / (ano_atual - ano_fabricacao)
+
+km_2020 = km_total + km_media
+print(km_2020)
+
+# -------------------------------------#
+
+# Podemos delcarar as variaveis em forma de lista
+
+ano_atual, ano_fabricacao, km_total = 2022, 2003, 44410.0
